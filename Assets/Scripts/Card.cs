bf004cedd3754604ae3e0feb99c32df513d78f62
Dos06 <dostermorgan815@gmail.com>
2021-02-16 19:24:19 +0600

    Add scene and cards' position

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private GameObject CardBack;
+    public void OnMouseDown()
+    {
+        if (CardBack.activeSelf)
+        {
+            CardBack.SetActive(false);
+            
+        }
+    }
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public const int rows = 2;
+    public const int cols = 4;
+    public const float offsetX = 4f;
+    public const float offsetY = 5f;
+
+    [SerializeField] private Card card;
+    [SerializeField] private Sprite[] images;
+
+    private void Start()
+    {
+        Vector3 startPosition = card.transform.position;
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Card newCard;
+                if (i == 0 && j == 0)
+                {
+                    newCard = card;
+                }
+                else
+                {
+                    newCard = Instantiate(card) as Card;
+                }
+
+                int index = j * cols + i;
+                int id = numbers[index];
+                newCard.ChangeSprite(id, images[id]);
+
+                float positionX = (offsetX * i) + startPosition.x;
+                float positionY = (offsetY * j) + startPosition.y;
+                newCard.transform.position = new Vector3(positionX, positionY, startPosition.z);
+            }
+        }
+    }
+
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int temp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = temp;
+        }
+        return newArray;
+    }
+
+}
